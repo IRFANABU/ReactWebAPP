d6f14f1918d0a252ab677dc244e5716f23efa6a4
IRFANABU <irfan10942@gmail.com>
2023-10-29 22:29:46 +0530

    ReactNodeNewProject

--- src/components/navbarelement.js
+++
@@ -1,5 +0,0 @@
-import {FaBars} from 'react-icons/fa'
-import styled from 'style-components';
-export const Bars = styled(FaBars)`
-display:'block'
-color:yellow`;
--- src/pages/Home/home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function home(props) {
-  return (
-    <div>
-      <h1 style={{textAlign:"center"}}>{props.Children}</h1>
-    </div>
-  )
-}
