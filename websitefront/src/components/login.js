41688c8d17240c31e5e3ba45a4253b8905402a23
IRFANABU <irfan10942@gmail.com>
2023-10-30 17:47:17 +0530

    connect dront and Backend

--- websitefront/src/components/login.js
+++ websitefront/src/components/login.js
@@ -1,27 +1,57 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import { NavLink as Link,  useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import { NavLink as Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import Validation from './loginValidation';
+// // const navigate = useNavigate(); 
+// const handleSubmit = (event) =>{
+//     event.preventDefault()
+//     // navigate('/home')  
+// }
 
- 
-// const navigate = useNavigate(); 
-const handleSubmit = (event) =>{
-    event.preventDefault()
-    // navigate('/home')  
-}
- 
 function Login() {
-  
+  const navigate = useNavigate();
+  // usestate hook to change the values in function component
+  const [values, setValues] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState({});
+  const handleValues = (event) => {
+    const typeofInput = event.target.name;
+    setValues(userValues => ({ ...userValues, [typeofInput]: [event.target.value] }));
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //  validation for form 
+    setErrors(Validation(values));
+    if (errors.email === '' && errors.password === '') {
+      axios.post("http://localhost:8081/login", values).then(res => {
+
+        if (res.data === "success") {
+          navigate('/navbar')
+        }
+        else {
+          setErrors(Validation(values));
+          console.log("errrrrr")
+
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+    //  console.log(values)
+  }
+
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
-      <form action=''  onSubmit={handleSubmit} className='bg-white p-3 rounded w-25'>
+      <form action='' onSubmit={handleSubmit} className='bg-white p-3 rounded w-25'>
         <h2>LOGIN</h2>
         <div className='mb-3'>
-            <label htmlFor='email'>Email</label>
-            <input type='email' name='email' placeholder='Email' className='form-control'/>
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' placeholder='Email' className='form-control' onChange={handleValues} />
+          {errors.email && <span className='text-danger'>{errors.email}</span>}
         </div>
         <div className='mb-3'>
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' placeholder='Password' className='form-control'/>
+          <label htmlFor='password'>Password</label>
+          <input type='password' name='password' placeholder='Password' className='form-control' onChange={handleValues} />
         </div>
         <button type='submit' className='btn btn-success w-100'>Log In</button>
         <p className='text-center my-2'>Create an New Account</p>
--- websitefront/src/components/signup.js
+++ websitefront/src/components/signup.js
@@ -1,29 +1,52 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
+import signupValidation from './signupValidation'
 
 
+function Signup() {
+  const navigate = useNavigate();
+  // const userData = { name: '', email: '', password: '' }
+  const [values, setValues] = useState({ name: '', email: '', password: ''})
+  const [errors,setErrors]= useState({});
 
-const handleSignupForm = (event) =>{
-        event.preventDefault();
-}
-
-
-function signup() {
+  const handleInputs = (event) => {
+    const type = event.target.name
+    const valueofall = event.target.value
+    setValues((userValues) => ({...userValues , [type]: valueofall }))
+  }
+  const handleSignupForm = (event) => {
+    event.preventDefault();
+    setErrors(signupValidation(values));
+    if(errors.name === "" && errors.email === "" && errors.password === ""){
+     axios.post("http://localhost:8081/signup",values)
+      .then(res=>{
+        alert("sucess")
+        navigate('/');
+        console.log(res)
+      }).catch(err=>console.log(err))
+    }
+    // console.log(values);
+  }
   return (
     <div  className='d-flex justify-content-center align-items-center vh-100'>
      <form action='' className='w-25 bg-light p-3 rounded' onSubmit={handleSignupForm}>
      <h2>SIGNUP</h2>
      <div className='mb-3'>
             <label htmlFor='name'>Name</label>
-            <input type='text' name='name' placeholder='Name' className='form-control'/>
+            <input type='text' name='name' placeholder='Name' className='form-control' onChange={handleInputs}/>
+            {errors.name && <span className='text-danger'>{errors.name}</span>}
         </div>
         <div className='mb-3'>
             <label htmlFor='email'>Email</label>
-            <input type='email' name='email' placeholder='Email' className='form-control'/>
+            <input type='email' name='email' placeholder='Email' className='form-control'onChange={handleInputs}/>
+            {errors.email && <span className='text-danger'>{errors.email}</span>}
         </div>
         <div className='mb-3'>
             <label htmlFor='password'>Password</label>
-            <input type='password' name='password' placeholder='Password' className='form-control'/>
+            <input type='password' name='password' placeholder='Password' className='form-control' onChange={handleInputs}/>
+            {errors.password && <span className='text-danger'>{errors.password}</span>}
         </div>
         <button className='btn btn-primary w-100'>Register</button>
         <div className='my-2'>
@@ -34,4 +57,4 @@ function signup() {
   )
 }
 
-export default signup
+export default Signup;
