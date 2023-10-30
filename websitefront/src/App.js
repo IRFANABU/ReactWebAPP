3747f722ca74e788869cf8b760f0903069269c84
IRFANABU <irfan10942@gmail.com>
2023-10-30 10:02:52 +0530

    Login and SignUp

--- websitefront/src/App.js
+++ websitefront/src/App.js
@@ -6,12 +6,31 @@ import Home from './pages/Home/home';
 import Footer from './components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
+import Login from './components/login';
+import Signup from './components/signup'
 import './App.css';
 function App() {
   return (
     // fragment using in react <>  </> multiple tag
     <>
     <Routers>
+    {/* <Navbar signBtn="Sign In"/>
+    <Routes>
+      <Route path='/' element={<Home  Children="its our Home page"/>}></Route>
+      <Route path='/home' element={<Home Children="its our home page"/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About title="aboutone"/>}/>
+    </Routes>
+    
+    <Footer details1="hello" details2="hello" details3="hello"/> */}
+        {/* <Login/> */}
+
+
+    <Routes>
+    <Route path='/' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
     <Navbar signBtn="Sign In"/>
     <Routes>
       <Route path='/' element={<Home  Children="its our Home page"/>}></Route>
@@ -20,12 +39,11 @@ function App() {
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/about' element={<About title="aboutone"/>}/>
     </Routes>
-    <Footer 
-    details1="hello"
-    details2="hello"
-    details3="hello"
-    />
+    
+    <Footer details1="hello" details2="hello" details3="hello"/>
+
     </Routers>
+  
     </>
    
   );
